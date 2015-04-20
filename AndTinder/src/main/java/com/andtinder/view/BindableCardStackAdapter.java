158415c07bf65419181e8047c7c2660f680dcb1f
Sasa Sekulic <ja@sasasekulic.info>
2015-04-20 17:50:55 +0200

    added BindableCardStackAdapter

---
+++ AndTinder/src/main/java/com/andtinder/view/BindableCardStackAdapter.java
@@ -0,0 +1,50 @@
+package com.andtinder.view;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class BindableCardStackAdapter extends CardStackAdapter {
+
+    private List<AdapterDataBinder> mAdapterDataBinders;
+
+    public interface AdapterDataBinder {
+
+        public boolean canBind(int position, Object model);
+
+        public View doBind(int position, Object model, View convertView, ViewGroup parent);
+
+    }
+
+    public BindableCardStackAdapter(Context mContext) {
+        super(mContext);
+        mAdapterDataBinders = new ArrayList<AdapterDataBinder>();
+    }
+
+    public void registerAdapterDataBinder(AdapterDataBinder adapterDataBinder) {
+        if (!mAdapterDataBinders.contains(adapterDataBinder)) {
+            mAdapterDataBinders.add(adapterDataBinder);
+        }
+    }
+
+    public void unregisterAdapterDataBinder(AdapterDataBinder adapterDataBinder) {
+        if (mAdapterDataBinders.contains(adapterDataBinder)) {
+            mAdapterDataBinders.remove(adapterDataBinder);
+        }
+    }
+
+    @Override
+    public View getCardView(int position, Object model, View convertView, ViewGroup parent) {
+
+        for (AdapterDataBinder binder : mAdapterDataBinders) {
+            if (binder.canBind(position, model)) {
+                return binder.doBind(position, model, convertView, parent);
+            }
+        }
+
+        return convertView;
+    }
+}

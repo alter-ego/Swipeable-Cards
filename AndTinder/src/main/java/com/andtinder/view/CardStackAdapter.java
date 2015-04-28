6f4496c25ed0425363e63f3b7b000b779fe168c1
mrsasha <ja@sasasekulic.info>
2015-04-28 20:04:45 +0200

    removed BaseCardStackAdapter; added stable ids, linkedlist to CardStackAdapter

--- AndTinder/src/main/java/com/andtinder/view/CardStackAdapter.java
+++ AndTinder/src/main/java/com/andtinder/view/CardStackAdapter.java
@@ -5,123 +5,132 @@ import com.andtinder.R;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
-public abstract class CardStackAdapter extends BaseCardStackAdapter {
-	private final Context mContext;
+public abstract class CardStackAdapter extends BaseAdapter {
 
-	/**
-	 * Lock used to modify the content of {@link #mData}. Any write operation
-	 * performed on the deque should be synchronized on this lock.
-	 */
-	private final Object mLock = new Object();
-	private ArrayList<Object> mData;
-        private int mWrapperBackgroundResource = R.drawable.card_bg;
+    private final Context mContext;
 
-	public CardStackAdapter(Context context) {
-		mContext = context;
-		mData = new ArrayList<Object>();
+    /**
+     * Lock used to modify the content of {@link #mData}. Any write operation
+     * performed on the deque should be synchronized on this lock.
+     */
+    private final Object mLock = new Object();
+
+    private List<Object> mData;
+
+    private int mWrapperBackgroundResource = R.drawable.card_bg;
+
+    public CardStackAdapter(Context context) {
+	mContext = context;
+	mData = new LinkedList<>();
+    }
+
+    public CardStackAdapter(Context context, Collection<Object> items) {
+	mContext = context;
+	mData = new LinkedList<Object>(items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+	FrameLayout wrapper = (FrameLayout) convertView;
+	FrameLayout innerWrapper;
+	View cardView;
+	View convertedCardView;
+	if (wrapper == null) {
+	    wrapper = new FrameLayout(mContext);
+	    wrapper.setBackgroundResource(mWrapperBackgroundResource);
+	    if (shouldFillCardBackground()) {
+		innerWrapper = new FrameLayout(mContext);
+		innerWrapper.setBackgroundColor(mContext.getResources().getColor(R.color.card_bg));
+		wrapper.addView(innerWrapper);
+	    } else {
+		innerWrapper = wrapper;
+	    }
+	    cardView = getCardView(position, getCardModel(position), null, parent);
+	    innerWrapper.addView(cardView);
+	} else {
+	    if (shouldFillCardBackground()) {
+		innerWrapper = (FrameLayout) wrapper.getChildAt(0);
+	    } else {
+		innerWrapper = wrapper;
+	    }
+	    cardView = innerWrapper.getChildAt(0);
+	    convertedCardView = getCardView(position, getCardModel(position), cardView, parent);
+	    if (convertedCardView != cardView) {
+		wrapper.removeView(cardView);
+		wrapper.addView(convertedCardView);
+	    }
 	}
 
-	public CardStackAdapter(Context context, Collection<Object> items) {
-		mContext = context;
-		mData = new ArrayList<Object>(items);
+	return wrapper;
+    }
+
+    protected abstract View getCardView(int position, Object model, View convertView, ViewGroup parent);
+
+    public boolean shouldFillCardBackground() {
+	return true;
+    }
+
+    public void add(Object item) {
+	synchronized (mLock) {
+	    mData.add(item);
 	}
+	notifyDataSetChanged();
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		FrameLayout wrapper = (FrameLayout) convertView;
-		FrameLayout innerWrapper;
-		View cardView;
-		View convertedCardView;
-		if (wrapper == null) {
-			wrapper = new FrameLayout(mContext);
-			wrapper.setBackgroundResource(mWrapperBackgroundResource);
-			if (shouldFillCardBackground()) {
-				innerWrapper = new FrameLayout(mContext);
-				innerWrapper.setBackgroundColor(mContext.getResources().getColor(R.color.card_bg));
-				wrapper.addView(innerWrapper);
-			} else {
-				innerWrapper = wrapper;
-			}
-			cardView = getCardView(position, getCardModel(position), null, parent);
-			innerWrapper.addView(cardView);
-		} else {
-			if (shouldFillCardBackground()) {
-				innerWrapper = (FrameLayout) wrapper.getChildAt(0);
-			} else {
-				innerWrapper = wrapper;
-			}
-			cardView = innerWrapper.getChildAt(0);
-			convertedCardView = getCardView(position, getCardModel(position), cardView, parent);
-			if (convertedCardView != cardView) {
-				wrapper.removeView(cardView);
-				wrapper.addView(convertedCardView);
-			}
-		}
-
-		return wrapper;
+    public void addAll(List<Object> items) {
+	synchronized (mLock) {
+	    mData.addAll(items);
 	}
+	notifyDataSetChanged();
+    }
 
-	protected abstract View getCardView(int position, Object model, View convertView, ViewGroup parent);
-
-	public boolean shouldFillCardBackground() {
-		return true;
+    public Object pop() {
+	Object model;
+	synchronized (mLock) {
+	    model = mData.remove(mData.size() - 1);
 	}
+	notifyDataSetChanged();
+	return model;
+    }
 
-	public void add(Object item) {
-		synchronized (mLock) {
-			mData.add(item);
-		}
-		notifyDataSetChanged();
+    @Override
+    public Object getItem(int position) {
+	return getCardModel(position);
+    }
+
+    public Object getCardModel(int position) {
+	synchronized (mLock) {
+	    return mData.get(mData.size() - 1 - position);
 	}
+    }
 
-        public void addAll(List<Object> items) {
-	        synchronized (mLock) {
-	                mData.addAll(items);
-	        }
-	        notifyDataSetChanged();
-        }
+    @Override
+    public int getCount() {
+	return mData.size();
+    }
 
-	public Object pop() {
-	    Object model;
-		synchronized (mLock) {
-			model = mData.remove(mData.size() - 1);
-		}
-		notifyDataSetChanged();
-		return model;
-	}
+    @Override
+    public long getItemId(int position) {
+	return getItem(position).hashCode();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return getCardModel(position);
-	}
+    public Context getContext() {
+	return mContext;
+    }
 
-	public Object getCardModel(int position) {
-		synchronized (mLock) {
-			return mData.get(mData.size() - 1 - position);
-		}
-	}
+    @Override
+    public boolean hasStableIds() {
+	return true;
+    }
 
-	@Override
-	public int getCount() {
-		return mData.size();
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return getItem(position).hashCode();
-	}
-
-	public Context getContext() {
-		return mContext;
-	}
-
-        public void setWrapperBackgroundResource(int resourceId) {
-                mWrapperBackgroundResource = resourceId;
-        }
+    public void setWrapperBackgroundResource(int resourceId) {
+	mWrapperBackgroundResource = resourceId;
+    }
 }
